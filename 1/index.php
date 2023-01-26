ea28f98e8d5864df3c0e60ae2fae6d688210ab47
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-01-26 10:21:49 +0100

    Update index.php

--- 1/index.php
+++ 1/index.php
@@ -1,3 +1,8 @@
 <?php
+declare(strict_types=1);
 echo("Welcome to page 1");
+function combine(string $one, string $two){
+    return $one . $two;
+}
+echo combine("One plus", " two equals three");
 ?>
