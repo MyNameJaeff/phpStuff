bb8c23fc4a79164a5a8a70a17b37a37247077ab2
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-02-08 20:38:23 +0100

    Alot

---
+++ FormUppgift/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Form uppgift</title>
+</head>
+<body>
+    <div class="allt">
+        <div class="TitleBox">
+            <h1>PHP FORM</h1>
+        </div>
+        <div class="FormBox">
+            <form action="#" method="post" id="form" enctype="multipart/form-data">
+                <label for="firstName">First Name:</label>
+                <input type="text" id="firstName" name="firstName" required><br>
+                <label for="lastName">Last Name:</label>
+                <input type="text" id="lastName" name="lastName" required><br>
+                <label for="birthDate">Birth Date:</label>
+                <input type="text" id="birthDate" name="birthDate" required><br>
+                <input type="file" class="file" id="file" name="file" accept="image/png, image/jpeg, image/jpg, image/gif" required><br>
+                <input type="submit" id="sumbitForm" name="submitForm" value="Submit">
+            </form>
+        </div>
+        <div class="OutputBox">
+            <div class="ImageBox">
+            <?php
+            if(isset($_POST['submitForm'])){
+                $fileName = $_FILES['file']['name'];  
+                $temp_name = $_FILES['file']['tmp_name'];
+                if(isset($fileName) and !empty($fileName)){
+                    $location = 'uploads/';      
+                    if(move_uploaded_file($temp_name, $location.$fileName)){
+                        echo '<img src="'.$location.$fileName.'" alt="ERROR">';
+                    }
+                } else {
+                    echo 'You should select a file to upload !!';
+                }
+                
+            }
+            ?>
+            </div>
+            <div class="textBox">
+                <?php
+                if(isset($_POST['submitForm'])){
+                    $firstName = $_POST['firstName'];
+                    $lastName = $_POST['lastName'];
+                    $birthDate = $_POST['birthDate'];
+                    echo('<h3>'.$firstName.' '.$lastName.'</h3>');
+                    echo('<h3>'.$birthDate.'</h3>');
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
