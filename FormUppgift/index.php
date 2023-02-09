2d0476bbc3edccf3964175a29c155786d347adb8
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-02-09 09:52:04 +0100

    a

--- FormUppgift/index.php
+++ FormUppgift/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Form uppgift</title>
 </head>
@@ -13,15 +14,15 @@
             <h1>PHP FORM</h1>
         </div>
         <div class="FormBox">
-            <form action="#" method="post" id="form" enctype="multipart/form-data">
+            <form action="#" method="post" id="form" enctype="multipart/form-data" class="form">
                 <label for="firstName">First Name:</label>
                 <input type="text" id="firstName" name="firstName" required><br>
                 <label for="lastName">Last Name:</label>
                 <input type="text" id="lastName" name="lastName" required><br>
                 <label for="birthDate">Birth Date:</label>
                 <input type="text" id="birthDate" name="birthDate" required><br>
-                <input type="file" class="file" id="file" name="file" accept="image/png, image/jpeg, image/jpg, image/gif" required><br>
-                <input type="submit" id="sumbitForm" name="submitForm" value="Submit">
+                <input type="file" class="file form-control" id="file" name="file" accept="image/png, image/jpeg, image/jpg, image/gif" required><br>
+                <input type="submit" id="sumbitForm" name="submitForm" value="Show" class="submit">
             </form>
         </div>
         <div class="OutputBox">
@@ -30,10 +31,10 @@
             if(isset($_POST['submitForm'])){
                 $fileName = $_FILES['file']['name'];  
                 $temp_name = $_FILES['file']['tmp_name'];
-                if(isset($fileName) and !empty($fileName)){
-                    $location = 'uploads/';      
+                if(isset($fileName) && !empty($fileName)){
+                    $location = 'uploads/';
                     if(move_uploaded_file($temp_name, $location.$fileName)){
-                        echo '<img src="'.$location.$fileName.'" alt="ERROR">';
+                        echo '<img src="'.$location.$fileName.'" alt="Chosen Image">';
                     }
                 } else {
                     echo 'You should select a file to upload !!';
