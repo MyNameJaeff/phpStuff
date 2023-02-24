0ffd9b1b5356672a5d6746beddb18c18ec081e1f
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-02-24 10:37:11 +0100

    a

--- övn5/index.php
+++ övn5/index.php
@@ -4,17 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" class="href">
     <title>Databas uppgift</title>
 </head>
 <body>
-    <form action="#" method="post" enctype="multipart/form-data">
-        <label for="name">Namn: </label>
-        <input type="text" name="name" id="name" required> <br>
-        <label for="bdate">Födelsedatum: </label>
-        <input type="date" name="bdate" id="bdate" required> <br>
-        <input type="file" name="file" id="file" accept="image/png, image/jpeg, image/jpg, image/gif" required><br>
-        <input type="submit" value="Submit" name="submit">
-    </form><br>
+    <div>
+        <form action="#" method="post" enctype="multipart/form-data" class="form">
+            <div class="mb-1">
+                <label for="name" class="form-label">Namn: </label>
+                <input type="text" name="name" id="name" required class="form-control" > <br>
+            </div>
+            <div class="mb-1">
+                <label for="bdate" class="form-label">Födelsedatum: </label>
+                <input type="date" name="bdate" id="bdate" required class="form-control" > <br>
+            </div>
+            <div class="mb-3 input-group">
+                <input type="file" name="file" id="file" accept="image/png, image/jpeg, image/jpg, image/gif" required class="form-control" ><br>
+            </div>
+            <input type="submit" value="Submit" name="submit" class="btn btn-primary">
+        </form><br>
     <?php
     $servername = "localhost";
     $username = "root";
@@ -64,8 +73,9 @@
         $conn->close();
     }
     ?>
-    <form action="table.php" method="post">
-        <input type="submit" value="Go next">
-    </form>
+        <form action="table.php" method="post">
+            <input type="submit" value="Go next">
+        </form>
+    </div>
 </body>
 </html>
--- övn5/table.php
+++ övn5/table.php
@@ -4,49 +4,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Table</title>
 </head>
 <body>
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "user_database";
-    
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-    
-    $sql = "SELECT id, fullname, birth_date, image_path FROM users";
-    $result = $conn->query($sql);
-    
-    $table = '<table border="1" cellpadding="10">';
-    $table .= "<tr>
-                    <td> Id </td>
-                    <td> Name </td>
-                    <td> Birth Date </td>
-                    <td> Image </td>
-                    </tr>";
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            $table .= "<tr><td>" . 
-            $row["id"] . "</td><td>" . 
-            $row["fullname"] . "</td><td>" . 
-            $row["birth_date"] . "</td><td class='imageBox'>" . "<img src='" . 
-            $row["image_path"] . "' alt='uploadedImage'>" . "</td></tr>";
+    <div>
+      <?php
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $dbname = "user_database";
+      
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
       }
-    } else {
-      echo "0 results";
-    }
-    $table .= "</table>";
-    echo($table);
-    $conn->close();
-    ?>
-    <br>
-    <form action="index.php" method="post">
-        <input type="submit" value="Go back">
-    </form>
+      
+      $sql = "SELECT id, fullname, birth_date, image_path FROM users";
+      $result = $conn->query($sql);
+      
+      $table = '<table class="table table-striped table-hover">';
+      $table .= "<tr>
+                      <td> Id </td>
+                      <td> Name </td>
+                      <td> Birth Date </td>
+                      <td> Image </td>
+                      </tr>";
+      if ($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()) {
+              $table .= "<tr><td>" . 
+              $row["id"] . "</td><td>" . 
+              $row["fullname"] . "</td><td>" . 
+              $row["birth_date"] . "</td><td class='imageBox'>" . "<img src='" . 
+              $row["image_path"] . "' alt='uploadedImage'>" . "</td></tr>";
+        }
+      } else {
+        echo "0 results";
+      }
+      $table .= "</table>";
+      echo($table);
+      $conn->close();
+      ?>
+      <br>
+      <form action="index.php" method="post">
+          <input type="submit" value="Go back">
+      </form>
+    </div>
 </body>
 </html>
